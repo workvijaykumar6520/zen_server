8a5501ddf188159e57860597d43f92919ba305e6
satyanarayanaRaju21 <raju@tilicho.in>
2024-07-31 22:35:21 +0530

    reference api

--- src/routes/users.py
+++ src/routes/users.py
@@ -5,6 +5,8 @@ def user_call(data):
     print(data)
     doc_ref = db.collection("users")
     doc_ref.add(data)
-    return {
-      "message": 'User created successfully'
-    }
+    usersStream = doc_ref.stream()
+    usersData = []
+    for user in usersStream:
+        usersData.append(user.to_dict())
+    return {"data": usersData, "message": "User created successfully"}
